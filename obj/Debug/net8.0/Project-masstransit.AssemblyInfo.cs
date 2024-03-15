549d9f10d7b680d87e08ca3f393b9cccb58e670c
Solid32 <guillaume.chinzi@hotmail.be>
2024-03-15 14:51:34 +0100

    gitignore

--- obj/Debug/net8.0/Project-masstransit.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project-masstransit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-masstransit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f255a0a877371736c42124bddc169f6006e81dc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b888e929ccd3f1591d621e107dce341ae815b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-masstransit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-masstransit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
